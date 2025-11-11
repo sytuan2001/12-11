7038bfb3402130fb555cb19fa1d8a0fa3081dd56
sytuan2001 <thuancat1978@gmail.com>
2025-11-12 00:09:43 +0700

    add

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 console.log('Happy developing ✨')
+// let year = parseInt(prompt("Enter a year"));
+// if (year % 4 == 0) {
+//     if (year % 100 == 0) {
+//         if (year % 400 == 0) {
+//             alert(year + " is a leap year");
+//         } else {
+//             alert(year + " is NOT a leap year");
+//         }
+//     } else {
+//         alert(year + " is a leap year");
+//     }
+// } else {
+//     alert(year + " is NOT a leap year");
+// }
+function kiemTraNamNhuan() {
+    let nam = parseInt(document.getElementById('nam').value);
+    let namNhuan = false;
+    if (nam %4 == 0 && nam %100 !=0) {
+        namNhuan = true;
+    }
+    if (nam % 100 == 0 && nam % 400 == 0) {
+        namNhuan = true;
+    }
+    if (namNhuan) {
+        document.getElementById('ketqua').innerHTML = nam + "là năm nhuận";
+    } else {
+        document.getElementById('ketqua').innerHTML = nam + "không phải năm nhuận";
+    }
+}
